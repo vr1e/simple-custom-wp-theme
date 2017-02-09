3d4a6c1018cb2609fa0092e99144d104b5e710a1
Nikola Vrhovac <vrllee@gmail.com>
2017-02-09 14:44:48 +0100

    Add post loop and custom body classes

--- simpletheme/header.php
+++ simpletheme/header.php
@@ -6,6 +6,15 @@
         <?php wp_head(); ?>
     </head>
 
-    <body>
+    <?php
+
+        if ( is_front_page() ):
+            $simpletheme_classes = array('simpletheme-class', 'my-class');
+        else:
+            $simpletheme_classes = array('no-simpletheme-class');
+        endif;
+    ?>
+
+    <body <?php body_class( $simpletheme_classes ); ?>>
 
         <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
