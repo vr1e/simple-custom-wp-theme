b0a0a5d6d1fc237130abf0a66e2ea24e23c6f12e
Nikola Vrhovac <vrllee@gmail.com>
2017-02-11 17:56:10 +0100

    Add post content hierarchy with bootstrap and post thumbnails

--- simpletheme/content.php
+++ simpletheme/content.php
@@ -1,10 +1,34 @@
-<!-- adds post title -->
-<h3><?php the_title(); ?></h3>
-<!-- adds featured image -->
-<div class="thumbnail-image"><?php the_post_thumbnail('medium'); ?></div>
-<!-- adds date, time and category -->
-<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g: i a'); ?>, in<?php the_category(); ?></small>
-<!-- adds post content -->
-<p><?php the_content(); ?></p>
+<!-- give posts article tag with wordpress generated ID and classes -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<hr>
+    <header class="entry-header">
+        <!-- adds post title linking permalink of the post -->
+        <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink()) ),'</a></h1>' ); ?>
+
+        <!-- adds date, time and category -->
+        <small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g: i a'); ?>, in<?php the_category(); ?></small>
+    </header>
+
+        <div class="row">
+            <!-- checks if post has featured image -->
+            <?php if( has_post_thumbnail() ): ?>
+
+            <div class="col-xs-12 col-sm-4">
+                <!-- adds featured image -->
+                <div class="thumbnail"><?php the_post_thumbnail('medium'); ?></div>
+            </div>
+            <div class="col-xs-12 col-sm-8">
+                <!-- adds post content -->
+                <?php the_content(); ?>
+            </div>
+
+            <?php else: ?>
+                <div class="col-xs-12 col-sm-12">
+                    <!-- adds post content -->
+                    <?php the_content(); ?>
+                </div>
+
+            <?php endif; ?>
+        </div>
+
+</article>
