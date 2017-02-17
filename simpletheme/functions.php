dc159764a7990809f7dd1149a535080bafeb5f7b
Nikola Vrhovac <vrllee@gmail.com>
2017-02-17 17:33:39 +0100

    Add custom hierarchical taxonomies

--- simpletheme/functions.php
+++ simpletheme/functions.php
@@ -120,10 +120,10 @@ function simple_custom_post_type() {
             'thumbnail',
             'revisions'
         ),
-        'taxonomies' => array(
-            'category',
-            'post_tag'
-        ),
+        // 'taxonomies' => array(
+        //     'category',
+        //     'post_tag'
+        // ),
         'menu_position' => 5,
         'exclude_from_search' => false
     );
@@ -133,3 +133,35 @@ function simple_custom_post_type() {
 }
 // initialize custom post type defined above
 add_action('init', 'simple_custom_post_type');
+
+function simple_custom_taxonomies() {
+
+    // add new taxonomy hierarchical
+    $labels = array(
+        'name' => 'Types',
+        'singular_name' => 'Type',
+        'search_items'  => 'Search Types',
+        'all_items' => 'All Types',
+        'parent_item' =>  'Parent Type',
+        'parent_item_colon' => 'Parent Type:',
+        'edit_item' => 'Edit Type',
+        'update_item' => 'Update Type',
+        'add_new_item' => 'Add New Type',
+        'new_item_name' => 'New Type Name',
+        'menu_name' => 'Type'
+    );
+
+    $args = array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array( 'slug' => 'type' )
+    );
+
+    register_taxonomy('type', array('portfolio'), $args);
+    // add new taxonomy NOT hierarchical
+}
+
+add_action( 'init', 'simple_custom_taxonomies' );
